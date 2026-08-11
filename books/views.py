067f73a931fbdb56b18bd06aa2aa939c321984d1
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-08-11 13:28:04 +0330

    update view for Code format

--- books/views.py
+++ books/views.py
@@ -9,9 +9,7 @@ from rest_framework import status
 class BookApiView(APIView):
     def get(self , request):
         books = Book.objects.all()
-
         data = []
-
         for book in books :
             data.append({
                 'id':book.id,
@@ -20,12 +18,13 @@ class BookApiView(APIView):
                 'year':book.year,
             })
         return Response(data)
+    
     def post(self , request):
         title = request.data.get('title')
         author = request.data.get('author')
         year = request.data.get('year')
-
         if not all([title , author , year]):
             return Response({'error':"تمام فیلد ها الزلمی است ."},status=status.HTTP_400_BAD_REQUEST)
+        
         book = Book.objects.create(title=title , author=author , year=int(year))
         return Response({'id':book.id , 'title':book.title , 'author':book.author , 'year':book.year} , status=status.HTTP_201_CREATED)
